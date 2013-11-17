e1b2b4bbcf6f17158f2b5cbbccf0b5766e0efbe0
jsoftbiz@gmail.com <pamela55>
2013-11-17 17:03:13 +0100

    Added multi attributes div tag validation

--- src/main/java/com/jsoft/HtmlAssert.java
+++ src/main/java/com/jsoft/HtmlAssert.java
@@ -1,6 +1,9 @@
 package com.jsoft;
 
-import com.jsoft.attributes.HtmlAttribute;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * DSL based assertion utility to assert HTML path and content
@@ -15,7 +18,7 @@ import com.jsoft.attributes.HtmlAttribute;
  * Will be asserted by
  * String html = ... // content above
  * HtmlAssert htmlAssert = new HtmlAssert(html);
- * htmlAssert.table(id="mytable").tr(class="someclass").td(class="someclass", id="sometd").text("content");
+ * htmlAssert.table(id,"mytable").tr(class,"someclass").td(class,"someclass", id,"sometd").text("content");
  * <p/>
  * all parameters are optional, in case they are blank, HtmlAssert will try to find itself the path,
  * e.g. the following should work :
@@ -43,23 +46,66 @@ public class HtmlAssert {
     this(html, Parsing.LENIENT);
   }
 
-/*
-  public HtmlAssert div(HtmlAttribute attribute) {
-    // from attribute reconstruct html (<div attr=""...) order alphabetically
-    // search all "<div nb_attr"
-    // iterate and return position for one with all attributes
-
-  }
-*/
-
   public HtmlAssert div() {
     int pos = html.toLowerCase().indexOf("<div>");
-    if (parsing == Parsing.STRICT && pos == 0) {
-      return new HtmlAssert(html.substring(pos + "<div>".length()), parsing);
+    return assertTagPosition(pos);
+  }
+
+  public HtmlAssert div(final String... attributes) {
+    if ((attributes.length % 2) == 1) {
+      throw new AssertionError("div attributes should be defined in pair div(\"name\", \"value\", ...)");
     }
-    if (parsing == Parsing.LENIENT && pos > -1) {
-      return new HtmlAssert(html.substring(pos + "<div>".length()), parsing);
+    Map<String, String> attributesMap = new HashMap<String, String>();
+    for (int i = 0; i < attributes.length; i += 2) {
+      final String attributeName = attributes[i];
+      final String attributeValue = attributes[i+1];
+      attributesMap.put(attributeName, attributeValue);
+    }
+
+    Pattern tagsPattern = Pattern.compile("(<div.*?>)");
+    Matcher tagsMatcher = tagsPattern.matcher(html);
+    while (tagsMatcher.find()) {
+      Map<String, String> matchedAttributesMap = new HashMap<String, String>();
+      String currentTag = html.substring(tagsMatcher.start(), tagsMatcher.end());  // id="ss" class="cdcd" char=ssj
+      Pattern attributesPattern = Pattern.compile("(\\w+)(\\=\"*(\\w+)\"*)*");
+      Matcher attributesMatcher = attributesPattern.matcher(currentTag);
+      while (attributesMatcher.find()) {
+        if (!attributesMatcher.group(0).equalsIgnoreCase("div")) {
+          matchedAttributesMap.put(attributesMatcher.group(1), attributesMatcher.group(3));
+        }
+      }
+
+      if (hashMapsAreEqual(attributesMap, matchedAttributesMap)) {
+        int pos = tagsMatcher.start();
+        return assertTagPosition(pos);
+      }
+    }
+    return assertTagPosition(-1);
+  }
+
+  private HtmlAssert assertTagPosition(final int position) {
+    if (parsing == Parsing.STRICT && position == 0) {
+      return new HtmlAssert(html.substring(position + "<div>".length()), parsing);
+    }
+    if (parsing == Parsing.LENIENT && position > -1) {
+      return new HtmlAssert(html.substring(position + "<div>".length()), parsing);
     }
     throw new AssertionError("<div> is not present");
+  }
+
+  private boolean hashMapsAreEqual(final Map<String, String> map1, final Map<String, String> map2) {
+    if (map1.size() != map2.size()) {
+      return false;
+    }
+    for (String s : map1.keySet()) {
+      if (map1.get(s) == null) {
+        if (map2.get(s) != null) {
+          return false;
+        }
+      } else if (!map1.get(s).equalsIgnoreCase(map2.get(s))) {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- src/test/java/com/jsoft/HtmlAssertTest.java
+++ src/test/java/com/jsoft/HtmlAssertTest.java
@@ -3,9 +3,6 @@ package com.jsoft;
 
 import org.junit.Test;
 
-import static com.jsoft.attributes.HtmlAttribute.$;
-import static com.jsoft.attributes.HtmlAttribute.attributes;
-
 /**
  * @author Aurelien Broszniowski
  */
@@ -13,31 +10,15 @@ public class HtmlAssertTest {
 
   @Test
   public void testPassingDiv() {
-    String html = "<div><div id=\"someid\" class=\"someclass\" hidden></div></div>";
+    String html = "<div><div id=\"someid\" class=\"someclass\" hidden><div class=\"someclass\" ><div></div></div></div></div>";
     HtmlAssert htmlAssert = new HtmlAssert(html);
-    htmlAssert.div();
+
+    htmlAssert.div("id", "someid", "class", "someclass", "hidden", null).div("class", "someclass");
+    htmlAssert.div("hidden", null, "class", "someclass", "id", "someid");
   }
 
-/*
 
-  @Test
-  public void testPassingLenientDivWithAttributes() {
-    String html = "<div><div id=\"someid\" lang=\"fr\" hidden></div></div>";
-    HtmlAssert htmlAssert = new HtmlAssert(html);
-    htmlAssert.div().id("someid").lang("fr");
-
-//    TODO : implement this syntax in simple cases then complex cases (that one)
-    HtmlAssert.validate(
-        HtmlAssert.html(html)
-            .div().id("someid").lang("fr")
-            .div()
-            .div().id("someid").lang("fr")
-    );
-
-
-        .div($().id("someid").lang("fr").$());
-  }
-*/
+  // Empty Div tests
 
   @Test
   public void testPassingLenientEmptyDiv() {
@@ -66,4 +47,130 @@ public class HtmlAssertTest {
     HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
     htmlAssert.div();
   }
+
+  // Filled Div tests
+
+  @Test
+  public void testPassingLenientOrderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass");
+  }
+
+  @Test
+  public void testPassingLenientUnorderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("class", "someclass", "id", "someid");
+  }
+
+  @Test
+  public void testPassingLenientMultipleFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass").div("class", "someclass");
+  }
+
+  @Test
+  public void testPassingLenientMultipleMixedDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass").div();
+  }
+
+
+  @Test
+  public void testPassingStrictOrderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("id", "someid", "class", "someclass");
+  }
+
+  @Test
+  public void testPassingStrictUnorderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("class", "someclass", "id", "someid");
+  }
+
+  @Test
+  public void testPassingStrictMultipleFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("id", "someid", "class", "someclass").div("class", "someclass");
+  }
+
+  @Test
+  public void testPassingStrictMultipleMixedDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("id", "someid", "class", "someclass").div("class", "someclass").div();
+  }
+
+
+  @Test(expected = AssertionError.class)
+  public void testFailingLenientOrderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someNONclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass");
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testFailingLenientUnorderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someNONclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("class", "someclass", "id", "someid");
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testFailingLenientMultipleFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someNONclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass").div("class", "someclass");
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testFailingLenientMultipleMixedDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><dZv></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass").div();
+  }
+
+
+  @Test(expected = AssertionError.class)
+  public void testFailingStrictOrderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someNONclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("id", "someid", "class", "someclass");
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testFailingStrictUnorderedFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someNONclass\"><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("class", "someclass", "id", "someid");
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testFailingStrictMultipleFilledDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someNONclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("id", "someid", "class", "someclass").div("class", "someclass");
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testFailingStrictMultipleMixedDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\"><div class=\"someclass\" ><dZv></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html, Parsing.STRICT);
+    htmlAssert.div().div("id", "someid", "class", "someclass").div("class", "someclass").div();
+  }
+
+
+  @Test(expected = AssertionError.class)
+  public void testFailingLenientTooManyAttrDiv() {
+    String html = "<div><div id=\"someid\" class=\"someclass\" hidden><div class=\"someclass\" ><div></div></div></div></div>";
+    HtmlAssert htmlAssert = new HtmlAssert(html);
+    htmlAssert.div("id", "someid", "class", "someclass");
+  }
+
 }
